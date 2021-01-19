3937dc0c2eb75b56963c35c6fbf0e0360f52b603
Phoenix1747 <apertured@public-files.de>
2021-01-19 01:51:28 +0100

    load of misc improvements

--- fov_tilemap.gd
+++ fov_tilemap.gd
@@ -11,9 +11,6 @@ var end_point := Vector2() # This is the very last point in the map dictionary.
 var previous_camera_position := Vector2() # This var holds the previous camera position for "draw_map".
 var previous_camera_zoom := Vector2() # This var holds the previous camera zoom for "draw_map".
 
-var x_cell_values := []
-var y_cell_values := []
-
 func load_map(MapNode: TileMap): # This function generates the "map" dict for the tilemap "MapNode".
 	var used_cells = MapNode.get_used_cells()
 	
@@ -23,11 +20,9 @@ func load_map(MapNode: TileMap): # This function generates the "map" dict for th
 	for cell in used_cells: # Copy all the tile values into the dict.
 		map[cell.x][cell.y] = MapNode.get_cellv(cell)
 	
-	get_x_cell_values() # Grab all the x and y coords of the tilemaps cells.
-	get_y_cell_values()
-	
-	start_point = Vector2(x_cell_values.min(), y_cell_values.min()) # Compute start and end point of the tilemap.
-	end_point = Vector2(x_cell_values.max(), y_cell_values.max())
+	var rect = MapNode.get_used_rect() # Compute start and end point of the tilemap.
+	start_point = rect.position
+	end_point = rect.end
 
 
 # This function draws the tilemap in the Camera's Viewport.
@@ -38,30 +33,33 @@ func draw_map(MapNode: TileMap, CameraNode: Camera2D, CellMargin: int=0):
 		return # If the camera hasn't moved or zoomed in/out then return.
 	
 	var visible_res = CameraNode.get_viewport().size * CameraNode.zoom # Camera FOV aka all visible pixels.
-	var top_left = camera_pos - (visible_res / 2) # Camera Viewport top left point.
-	var bottom_right = top_left + visible_res # Camera Viewport bottom right point.
 	
-	# Convert all global world coordinates to map coordinates.
-	top_left = MapNode.world_to_map(top_left) - Vector2(CellMargin, CellMargin)
-	bottom_right = MapNode.world_to_map(bottom_right) + Vector2(CellMargin, CellMargin)
+	# Convert all global viewport world coordinates to map coordinates.
+	var top_left = MapNode.world_to_map(camera_pos - visible_res/2) - Vector2(CellMargin, CellMargin)
+	var bottom_right = MapNode.world_to_map(camera_pos + visible_res/2) + Vector2(CellMargin, CellMargin)
+	
+	var rect = Rect2(top_left, bottom_right - top_left) # Compute the (still) visible rect of tiles
+	var vis_rect = MapNode.get_used_rect().clip(rect)
 	
 	# Get all the cells that have a tile set to them.
-	# Clear only these cells if they are outside of the Camera FOV instead of everything.
+	# Clear only these cells and only if they are outside of the Camera FOV Rect.
 	# == only clear previously set and no longer visible cells.
 	for cell in MapNode.get_used_cells():
-		if cell.x < top_left.x or cell.x > bottom_right.x or cell.y < top_left.y or cell.y > bottom_right.y:
+		if not (vis_rect.has_point(cell) or  MapNode.get_cellv(cell) == -1):
 			MapNode.set_cellv(cell,-1)
 	
-	var x = top_left.x # top_left X coordinate that is added up 1 until it reaches bottom_right.x.
-	
+	var x = top_left.x # top_left X coordinate that is added up 1 until it reaches bottom_right.
+	vis_rect = MapNode.get_used_rect()
+
 	# Go through all cells in the camera FOV and do the magic.
 	while x <= bottom_right.x:
-		if x >= start_point.x and x <= end_point.x: # Check if the X value is in the bounds of the saved map dict.
-			var y = top_left.y # top_left Y coordinate that is added up 1 until it reaches bottom_right.y.
+		if map.has(x): # Check if the X value is in the bounds of the saved map dict.
+			var y = top_left.y # top_left Y coordinate that is added up 1 until it reaches bottom_right.
 			while y <= bottom_right.y:
-				if MapNode.get_cell(x,y) == -1: # Only set cells that are new to the Camera FOV.
-					if y >= start_point.y and y <= end_point.y: # Check if the Y value is in the bounds of the map.
-						MapNode.set_cell(x, y, map[x][y])
+				if not vis_rect.has_point(Vector2(x,y)): # Only do points that are visible.
+					if map[x].has(y): # Check if the Y value is in the bounds of the saved map dict.
+						if MapNode.get_cell(x,y) == -1: # Only set cells that are new to the Camera FOV.
+							MapNode.set_cell(x, y, map[x][y])
 				y += 1
 		x += 1
 	
@@ -75,26 +73,8 @@ func update_cell(MapNode: TileMap, PositionVector: Vector2, TileID: int):
 	MapNode.set_cellv(PositionVector, TileID)
 
 
-# This function gets all the X values for each cell from the tilemap.
-func get_x_cell_values():
-	for x in map.keys():
-		x_cell_values.append(int(x)) # Append all the X values to the array.
-
-
-# This function gets all the Y values for each cell from the tilemap.
-func get_y_cell_values():
-	var x_values = [] # New array that holds all the X values.
-	
-	for x in map.keys():
-		x_values.append(x) # Append all the X values to the array.
-	
-	for x in x_values:
-		for y in map[x].keys():
-			y_cell_values.append(int(y)) # Append all the Y values to the array.
-
-
 # This function re-builds the whole tilemap from the map dict.
 func rebuild_map(MapNode: TileMap):
-	for x in map:
+	for x in map.keys():
 		for y in map[x]:
 			MapNode.set_cell(x, y, map[x][y])
